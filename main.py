3a3c842ef789460bcc7d8572d74178f775f1eff0
ja5692 <39207432+ja5692@users.noreply.github.com>
2018-05-12 12:19:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-Python 3.5.3 (default, Jan 19 2017, 14:11:04) 
-[GCC 6.3.0 20170124] on linux
-Type "copyright", "credits" or "license()" for more information.
->>> 
+
 from Hologram.HologramCloud import HologramCloud
 import json
 import threading
