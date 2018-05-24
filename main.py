9693b7d0b30297591f629777b2100bfcbbb2fc1f
ja5692 <39207432+ja5692@users.noreply.github.com>
2018-05-23 20:48:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ import time
 import sys
 import Adafruit_DHT
 from RPIO import PWM
-mintemp = 30 #Minimum temperature required to trigger the "stove on" status
-sensor = Adafruit_DHT.DHT11 #Sensor type. 
-dhtpin = 4 #GPIO pin the DHT is connected to
+mintemp = 25                              #Minimum temperature required to trigger the "stove on" status
+sensor = Adafruit_DHT.DHT11               #Sensor type. 
+dhtpin = 4                                #GPIO pin the DHT is connected to
 
 
-credentials = {"devicekey":"2tVU6Pnf"} #Replace with your unique SIM device key
-#Instantiating a hologram instance
+credentials = {"devicekey":"2tVU6Pnf"}    #Replace with your unique SIM device key
+                                          #Instantiating a hologram instance
 hologram = HologramCloud(credentials, network='cellular', authentication_type="csrpsk")
 
 result = hologram.network.connect()
@@ -22,8 +22,8 @@ if result == False:
     print ' Failed to connect to cell network'
 else:
     print "Connection successful!"
-    print "Hologram online!"
-    #Enables Hologram to listen for incoming SMS messages
+    print "Hologram is online!"
+                                           #Enables Hologram to listen for incoming SMS messages
     recv = hologram.enableSMS()
 
 def update():
@@ -31,50 +31,50 @@ def update():
     humidity, temperature = Adafruit_DHT.read_retry(sensor, dhtpin)
     temperature = float('{0:0.1f}'.format(temperature))
 
-    #Determine if stove is on or off
+                                           #Determine if stove is on or off
     if temperature <= mintemp:
-        #hologram.sendMessage(json.dumps("Your stove is off." + "Temperature: " + temperature + "C"))
+                                           #hologram.sendMessage(json.dumps("Your stove is off." + "Temperature: " + temperature + "C"))
         print "Your stove is off. " + "Temperature: " + str(temperature) + "C"
         reply = hologram.sendSMS(phone, "Your stove is off. " + "Temperature: " + str(temperature) + "C")
 
     else:
-        #hologram.sendMessage(json.dumps("Your stove is on. Your home is at risk, Call authorities " + "Temperature: " + temperature + "C"))
-        print "Your stove is on. Your home is at risk, Call authorities " + "Temperature: " + str(temperature) + "C"
-        reply = hologram.sendSMS(phone, "Your stove is on. Your home is at risk, Call authorities " + "Temperature:  " + str(temperature) + "C")
-        #Waits for and processes user response to prompt
+                                           #hologram.sendMessage(json.dumps("Your stove is on. Your home is at risk. Please confirm this alert was recieved " + "Temperature: " + temperature + "C"))
+        print "Your stove is on. Your home is at risk. Please confirm this alert was recieved " + "Temperature: " + str(temperature) + "C"
+        reply = hologram.sendSMS(phone, "Your stove is on. Your home is at risk. Please confirm this alert was recieved " + "Temperature:  " + str(temperature) + "C")
+                                           #Waits for and processes user response to prompt
         count = 0
         while True:
-            #Reads user response
+                                           #Reads user response
             sms_obj = hologram.popReceivedSMS()
             if sms_obj is not None:
                 message = sms_obj.message.lower()
                 if message == "ok":
-                    #hologram.sendMessage(json.dumps("Please take necesary action, your home is at risk"))
-                    print "Please take necesary action, your home is at risk"
-                    reply = hologram.sendSMS(phone, "Please take necesary action, your home is at risk")
+                                           #hologram.sendMessage(json.dumps("Alert confirmed. Please take necesary action, your home is at risk"))
+                    print "Alert confirmed. Please take necesary action, your home is at risk"
+                    reply = hologram.sendSMS(phone, "Alert confirmed. Please take necesary action, your home is at risk")
                     break
 
                 elif message:
-                    #hologram.sendMessage(json.dumps("Please enter a valid response. (ok or ignore)"))
+                                           #hologram.sendMessage(json.dumps("Please enter a valid response. (ok or ignore)"))
                     print "Please enter a valid response. (ok or ignore)"
                     hologram.sendSMS(phone, "Please enter a valid response. (ok or ignore)")
                     count = 0
-                elif count >= 30:
-                    #hologram.sendMessage(json.dumps("No response recieved within 30 minutes, Alert Ignored. Reverting to standby mode."))
-                    print "No response recieved within 30 seconds, Alert Ignored. Reverting to standby mode."
-                    reply = hologram.sendSMS(phone, "No response recieved within 30 seconds, Alert Ignored. Reverting to standby mode.")
+                elif count >= 10:
+                                           #hologram.sendMessage(json.dumps("No response recieved within 10 minutes, Alert Ignored. Reverting to standby mode."))
+                    print "No response recieved within 10 minutes, Alert Ignored. Reverting to standby mode."
+                    reply = hologram.sendSMS(phone, "No response recieved within 10 minutes, Alert Ignored. Reverting to standby mode.")
                     break
             count += 1
             time.sleep(1)
 
-#Hologram waits for user input (standby mode)
+                                           #Hologram waits for user input (standby mode)
 while True:
     sms_obj = hologram.popReceivedSMS()
-    if sms_obj is not None: #If user sends something:
+    if sms_obj is not None:                #If user sends something:
         message = sms_obj.message
         phone = "+" + sms_obj.sender
 
-        if message.lower() in "status update": #If user enters keyword
+        if message.lower() in "status": #If user enters keyword
             update()
     time.sleep(1)
 
