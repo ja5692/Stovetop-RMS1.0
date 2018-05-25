480c79eef14d64c026c55f9188d29d15cb242b50
ja5692 <39207432+ja5692@users.noreply.github.com>
2018-05-24 22:46:48 -0400

    Update mq-51.py

--- mq-51.py
+++ mq-51.py
@@ -75,7 +75,7 @@ def readadc(adcnum, clockpin, mosipin, misopin, cspin):
 
         GPIO.output(cspin, True)
         
-        adcout >>= 1       # first bit is 'null' so drop it
+        adcout = 1       # first bit is 'null' so drop it
         return adcout
 #main ioop
 def main():
